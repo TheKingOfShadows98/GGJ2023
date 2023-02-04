3525757764e82444ebeb2355faa60453c5f82e17
Tk <thekingofshadows98@gmail.com>
2023-02-03 20:39:48 -0600

    Adding Comments :D

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Shooter : Spawner
 {
+    // Time Beetween shoots.
     [SerializeField, Tooltip("Tiempo entre disparo y disparo")] protected float delay;
 
-    [SerializeField] protected float timeToBullet;
+    // mark the next time to shoot a bullet.
+    protected float timeToBullet;
     private void Start()
     {
+        //Set the mark in "Dekay" times after.
         timeToBullet = Time.time + delay;
     }
     protected void FixedUpdate()
     {
+        // If Current Time is more or iquals to the mark, reset Timer and Spawn Bullet
         if (Time.time >= timeToBullet)
         {
-            Debug.Log("AHHHH");
+            //Set the mark in "Dekay" times after.
             timeToBullet = Time.deltaTime + delay;
+            // Use The Spawner.Spawn to spawn a bullet.
             Spawn();
         }
     }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,10 +7,13 @@ public class Spawner : MonoBehaviour
     // Store a GameObject Patter to Instantiate
     [SerializeField, Tooltip("Guarda aquí el objeto que quieres que se Spawnee.")] protected GameObject prefab;
 
+    // Spawn obj in current Position
     public void Spawn() => Spawn(transform.position);
     public void Spawn( Vector3 position)
     {
+        // Instantiate a obj into Current Scene
         GameObject obj = Instantiate(prefab);
+        // Set Position to obj.
         obj.transform.position = position;
     }
 }
